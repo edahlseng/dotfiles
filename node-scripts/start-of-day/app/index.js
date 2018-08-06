f8398ed543bdd9b613e66bdd5c9fee18cadd9f64
Eric Dahlseng <edahlseng@yahoo.com>
2018-08-06 12:26:57 -0700

    Added Calendar manipulations to start-of-day

--- node-scripts/start-of-day/app/index.js
+++ node-scripts/start-of-day/app/index.js
@@ -4,15 +4,34 @@
 
 import Future from "fluture";
 import { pipeK } from "ramda";
+import applescript from "applescript";
 
 import {
 	executeCommandInheritStdout,
 	singlePrompt,
 } from "@eric.dahlseng/cli-tools";
 
+const runApplescript = Future.encaseN(applescript.execString);
+
 // -----------------------------------------------------------------------------
 // Actions
 // -----------------------------------------------------------------------------
+
+const setCalendarToTodayDayView = () =>
+	runApplescript(`
+tell application "Calendar"
+	switch view to day view
+	view calendar at (current date)
+end tell
+	`);
+
+const setCalendarToTodayWeekView = () =>
+	runApplescript(`
+tell application "Calendar"
+	switch view to week view
+	view calendar at (current date)
+end tell
+	`);
 
 const runStartOfDay = pipeK(
 	() => executeCommandInheritStdout("open -a Notes"),
@@ -25,6 +44,7 @@ const runStartOfDay = pipeK(
 			shouldContinue => (shouldContinue ? Future.of() : Future.reject())
 		),
 	() => executeCommandInheritStdout("open -a Calendar"),
+	() => setCalendarToTodayDayView(),
 	() =>
 		singlePrompt({
 			type: "confirm",
@@ -33,6 +53,7 @@ const runStartOfDay = pipeK(
 		}).chain(
 			shouldContinue => (shouldContinue ? Future.of() : Future.reject())
 		),
+	() => setCalendarToTodayWeekView(),
 	() =>
 		executeCommandInheritStdout(
 			"open https://github.com/pulls/review-requested https://github.com/pulls?utf8=✓&q=is:open+is:pr+review:changes_requested+reviewed-by:edahlseng+archived:false"
