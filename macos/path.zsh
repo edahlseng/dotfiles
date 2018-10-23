211eb2e44cee3ca554b14d1de7dc31df1d14fe2a
Eric Dahlseng <edahlseng@yahoo.com>
2018-10-23 09:40:23 -0700

    Updated PATH

--- macos/path.zsh
+++ macos/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/go/bin:$HOME/Library/Python/$(python3 --version | sed -E 's/Python\ (.*)\.[0-9]+/\1/')/bin:$PATH"
+export PATH="$PATH:$HOME/go/bin:$HOME/Library/Python/$(python3 --version | head -n 1 | sed -E 's/^(Python\ )?([0-9.]+)\.[0-9]+.*/\2/')/bin"
