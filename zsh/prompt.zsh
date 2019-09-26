0fd3a4bec05270597d87b755785989f32bd17938
Eric Dahlseng <edahlseng@yahoo.com>
2019-09-25 20:22:01 -0700

    feat: Add number of Git stashes to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,49 +3,63 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 if (( $+commands[git] )); then
-	git="$commands[git]"
+	git="${commands[git]}"
 else
 	git="/usr/bin/git"
 fi
 
 git_prompt_info() {
-	ref=$($git symbolic-ref HEAD 2>/dev/null) || ref=$($git describe --tags --exact-match 2>/dev/null) || return
+	ref=$(${git} symbolic-ref HEAD 2>/dev/null) || ref=$(${git} describe --tags --exact-match 2>/dev/null) || return
 
 	echo "${ref#refs/heads/}"
 }
 
 gitDirty() {
-	if $(! $git status -s &> /dev/null); then
+	if $(! ${git} status -s &> /dev/null); then
 		echo ""
 	else
-		if [[ $($git status --porcelain) == "" ]]
-		then
-			echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+		if [[ $(${git} status --porcelain) == "" ]]; then
+			echo "on %{${fg_bold[green]}%}$(git_prompt_info)%{${reset_color}%}"
 		else
-			echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+			echo "on %{${fg_bold[red]}%}$(git_prompt_info)%{${reset_color}%}"
 		fi
 	fi
 }
 
-needPush() {
-	if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]; then
-		number=$($git rev-list --count HEAD ^@{upstream} 2>/dev/null)
+unpushedAndStashes() {
+	local message=""
 
-		if [[ $number == 0 ]]; then
-			echo " "
-		else
-			echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
+	if [ $(${git} rev-parse --is-inside-work-tree 2>/dev/null) ]; then
+		local numberUnpushed=$(${git} rev-list --count HEAD ^@{upstream} 2>/dev/null || echo 0)
+		local numberOfStashes=$(${git} rev-list --walk-reflogs --count refs/stash 2>/dev/null || echo 0)
+
+		local transitionWord="with"
+
+		if [[ ${numberUnpushed} != 0 ]]; then
+			message="${message} with %{${fg_bold[magenta]}%}${numberUnpushed} unpushed%{${reset_color}%}"
+			transitionWord="and"
+		fi
+
+		if [[ ${numberOfStashes} != 0 ]]; then
+			local stashSuffix="es"
+			if [[ ${numberOfStashes} == 1 ]]; then
+				stashSuffix=""
+			fi
+
+			message="${message} ${transitionWord} %{$fg_bold[magenta]%}${numberOfStashes} stash${stashSuffix}%{${reset_color}%}"
 		fi
 	fi
+
+	echo "${message}"
 }
 
 directoryName() {
-	echo "%{$fg_bold[black]%}%0~/%{$reset_color%}"
+	echo "%{${fg_bold[black]}%}%0~/%{${reset_color}%}"
 }
 
-export PROMPT=$'\n$fg_bold[black]%m:%{$reset_color%} $(directoryName) $(gitDirty)$(needPush)\n› '
+export PROMPT=$'\n${fg_bold[black]}%m:%{${reset_color}%} $(directoryName) $(gitDirty)$(unpushedAndStashes)\n› '
 setPrompt () {
-	export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+	export RPROMPT="%{${fg_bold[cyan]}%}%{${reset_color}%}"
 }
 
 precmd() {
